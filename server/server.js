329e06dfa5d373719d49632e98c0a813b6b66343
CamCoder <camcam.95@icloud.com>
2020-08-21 20:07:20 -0500

    finished UPDATE route

--- server/server.js
+++ server/server.js
@@ -75,18 +75,23 @@ console.log(req.body);
 });
 
 //Update Restaurants
-app.put("/api/v1/restaurants/:id", (req,res) => {
+app.put("/api/v1/restaurants/:id", async (req,res) => {
 
+    try {
+        const results = await db.query("UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
+        [req.body.name, req.body.location, req.body.price_range, req.params.id]);
 
-    console.log(req.params.id);
-    console.log(req.body);
-    res.status(200).json({
+        console.log(results);
+        res.status(200).json({
         status: "success",
         data:{
-            restaurants: ["mcdonalds", "wendys", "pizza"]    
+            restaurants: results.rows[0]    
         }
         
     });
+    } catch (err) {
+        console.log(err);
+    }
 
 });
 
