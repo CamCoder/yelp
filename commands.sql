3f972dbf47f08151093d8dabca19366c34ed23dc
CamCoder <camcam.95@icloud.com>
2020-08-21 19:56:41 -0500

    Add node pg, finished 3 routes

---
+++ commands.sql
@@ -0,0 +1,4 @@
+INSERT INTO restaurants (name, location, price_range) values ('DQ', 'Texas', 3);
+INSERT INTO restaurants (name, location, price_range) values ('DQ', 'Texas', 3);
+INSERT INTO restaurants (name, location, price_range) values ('WingStop', 'USA', 2);
+INSERT INTO restaurants (name, location, price_range) values ('PizzaHUt', 'Chicago', 4);
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require("express");
+const db = require('./db')
 const morgan = require('morgan')
 const app = express();
 
@@ -8,41 +9,68 @@ app.use(express.json());
 
 
 //Get all Restaurants
-app.get("/api/v1/restaurants", (req,res) => {
-    res.status(200).json({
-        status: "success",
-        data:{
-            restaurants: ["mcdonalds", "wendys", "pizza"]    
-        }
-        
-    });
+app.get("/api/v1/restaurants", async (req,res) => {
+
+    try {
+        const results = await db.query("SELECT * FROM restaurants");
+        console.log(results.rows);
+        res.status(200).json({
+            status: "success",
+            results: results.rows.length,
+            data:{
+                restaurants: results.rows 
+            }  
+        });  
+    } catch (err) {
+       console.log(err);
+    }
 });
 
+
 //Get a Restaurants
-app.get("/api/v1/restaurants/:id", (req,res) => {
+app.get("/api/v1/restaurants/:id", async (req,res) => {
 
 
-    console.log(req.params);
-    res.status(200).json({
+    console.log(req.params.id);
+    try {
+        
+        const results = await db.query("select * from restaurants where id = $1", [req.params.id]);
+
+        res.status(200).json({
         status: "success",
         data:{
-            restaurants: ["mcdonalds", "wendys", "pizza"]    
+            restaurants: results.rows[0]
         }
-        
-    });
+            
+        });
+    } catch (err) {
+         console.log(err);
+    }
+
 });
 
 
 //Create a restaurants
-app.post("/api/v1/restaurants/", (req,res) => {
-    console.log(req.body);
-    res.status(201).json({
-        status: "success",
-        data:{
-            restaurants: ["mcdonalds", "wendys", "pizza"]    
-        }
-        
-    });
+app.post("/api/v1/restaurants/", async (req,res) => {
+console.log(req.body);    
+    try {
+        const results = await db.query("INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *", 
+        [req.body.name, req.body.location, req.body.price_range]);
+
+        res.status(201).json({
+            status: "success",
+            data:{
+                restaurants: results.rows[0]   
+            }
+            
+        });   
+
+
+    } catch (err) {
+        console.log(err);
+    }
+
+    
 
 });
 
